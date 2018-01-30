433c652856d3a41c7e5bdebd3df3259fb550ccd3
Zack <zack.zx@i2edu.cn>
2018-01-30 11:00:41 +0800

    Create podspec.

--- ZKCrypto/RSAUtil.m
+++ ZKCrypto/RSAUtil.m
@@ -260,7 +260,7 @@ static NSData *base64_decode(NSString *str){
                                &outlen
                                );
         if (status != 0) {
-            NSLog(@"SecKeyEncrypt fail. Error Code: %d", status);
+            NSLog(@"SecKeyEncrypt fail. Error Code: %d", (int)status);
             ret = nil;
             break;
         }else{
@@ -316,7 +316,7 @@ static NSData *base64_decode(NSString *str){
                                &outlen
                                );
         if (status != 0) {
-            NSLog(@"SecKeyEncrypt fail. Error Code: %d", status);
+            NSLog(@"SecKeyEncrypt fail. Error Code: %d", (int)status);
             ret = nil;
             break;
         }else{
